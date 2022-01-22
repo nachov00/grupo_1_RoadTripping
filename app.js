6df9f0dadd3160e12157b540d21ed39060318b27
V41KNU7 <lucaszanier@gmail.com>
2022-01-22 19:54:48 -0300

    Casi terminando el Header

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const path = require('path');
 
 
-
 app.listen( 4040, () => console.log("Server Online!"))
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+app.get("/header", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/header.html'));
 });
 
