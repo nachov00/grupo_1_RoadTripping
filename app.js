f504c374e3d884239fd601ccb7fce2f4568f6785
nachov00 <nachov00@hotmail.com>
2022-01-22 11:38:00 -0300

    cambios en footer

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const path = require('path');
 
 
 
-app.listen( 4040, () => console.log("Server Online!"))
+app.listen(3000, () => console.log("Server Online!"))
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get("/login", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
