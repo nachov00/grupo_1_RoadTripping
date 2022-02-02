61648d3510ec452b659da2bc0d9ef93f68725ecd
V41KNU7 <lucaszanier@gmail.com>
2022-02-02 16:44:04 -0300

    a mergear

--- app.js
+++ app.js
@@ -3,30 +3,23 @@ const app = express();
 const path = require('path');
 
 
-app.listen( 4040, () => console.log("Server Online!"))
+
+app.listen(3000, () => console.log("Server Online!"))
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get("/home", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get("/shopping", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/shopping.html'));
+app.get("/login", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.get("/us", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/us.html'));
+app.get("/registrarse", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get("/Term&Cond", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/terminos_condiciones.html'));
-});
-
-app.get("/Privacidad", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/aviso_privacidad.html'));
-});
-
-app.get("/header", function(req, res) {
-    res.sendFile(path.resolve(__dirname, './views/header.html'));
+app.get("/preregistro", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/preregister.html'));
 });
