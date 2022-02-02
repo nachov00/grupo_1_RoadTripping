5cb85c6c31ab3a1eefb12a41b4b12d4705c8836a
V41KNU7 <lucaszanier@gmail.com>
2022-02-02 15:42:48 -0300

    Listo para el Merge Sprint-2

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ app.listen( 4040, () => console.log("Server Online!"))
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
+app.get("/home", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get("/shopping", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/shopping.html'));
+});
+
+app.get("/us", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/us.html'));
+});
+
+app.get("/Term&Cond", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/terminos_condiciones.html'));
+});
+
+app.get("/Privacidad", function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/aviso_privacidad.html'));
+});
+
 app.get("/header", function(req, res) {
     res.sendFile(path.resolve(__dirname, './views/header.html'));
 });
-
