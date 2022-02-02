6ee52155f9a2af299684a6d39253c9516314c8eb
V41KNU7 <lucaszanier@gmail.com>
2022-02-02 20:13:23 -0300

    Listo para la entrega Sprint-2

--- app.js
+++ app.js
@@ -9,30 +9,38 @@ app.listen(3000, () => console.log("Server Online!"))
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.get("/registrarse", function(req, res) {
+app.get("/registrarse", function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get("/preregistro", function(req, res) {
+app.get("/preregistro", function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/preregister.html'));
 });
 
-app.get('/Product-Detail', (req,res)=>{
+app.get('/Product-Detail', (req, res) => {
     res.sendFile(__dirname + '/views/productDetail2.html');
 });
 
-app.get('/Carrito', (req,res)=>{
+app.get('/Carrito', (req, res) => {
     res.sendFile(__dirname + '/views/shopping.html');
 });
 
-app.get('/Nosotros', (req,res)=>{
+app.get('/Nosotros', (req, res) => {
     res.sendFile(__dirname + '/views/us.html');
+});
+
+app.get('/Term&Cond', (req, res) => {
+    res.sendFile(__dirname + '/views/terminos_condiciones.html');
+});
+
+app.get('/Privacidad', (req, res) => {
+    res.sendFile(__dirname + '/views/aviso_privacidad.html');
 });
