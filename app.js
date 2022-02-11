29394e0a14a2292fcf5e6771c4314ef0ef3b8028
V41KNU7 <lucaszanier@gmail.com>
2022-02-11 18:29:43 -0300

    Listo?

--- app.js
+++ app.js
@@ -3,44 +3,47 @@ const app = express();
 const path = require('path');
 app.use(express.static('public'));
 
+let port = 3030;
 
-
-app.listen(6789, () => console.log("Server Online!"))
+app.listen(port, function() { 
+    console.log("Server Online!")
+    console.log("Port: "+port)
+})
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/main/home.html'));
 });
 
 app.get("/login", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/users/login.html'));
 });
 
 app.get("/registrarse", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/users/register.html'));
 });
 
 app.get("/preregistro", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/preregister.html'));
+    res.sendFile(path.resolve(__dirname, './views/users/preregister.html'));
 });
 
 app.get('/Product-Detail', (req, res) => {
-    res.sendFile(__dirname + '/views/productDetail2.html');
+    res.sendFile(__dirname + '/views/products/productDetail2.html');
 });
 
 app.get('/Carrito', (req, res) => {
-    res.sendFile(__dirname + '/views/shopping.html');
+    res.sendFile(__dirname + '/views/products/shopping.html');
 });
 
 app.get('/Nosotros', (req, res) => {
-    res.sendFile(__dirname + '/views/us.html');
+    res.sendFile(__dirname + '/views/main/us.html');
 });
 
 app.get('/Term&Cond', (req, res) => {
-    res.sendFile(__dirname + '/views/terminos_condiciones.html');
+    res.sendFile(__dirname + '/views/users/erminos_condiciones.html');
 });
 
 app.get('/Privacidad', (req, res) => {
-    res.sendFile(__dirname + '/views/aviso_privacidad.html');
+    res.sendFile(__dirname + '/views/users/aviso_privacidad.html');
 });
