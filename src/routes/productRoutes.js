d6dddd977e1de6737bda6112855264afc20f3859
Lis Perdomo <ing.lisperdomo@gmail.com>
2022-03-04 20:43:12 -0300

    linkeando product list

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController');
 
-
+router.get('/Listado', productController.listado);
 router.get('/Detail', productController.detail);
 router.get('/Shopping', productController.shopping);
 
