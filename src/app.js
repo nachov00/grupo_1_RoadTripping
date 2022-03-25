173a4b15617d363ce3f26ffbf9686698de2addae
V41KNU7 <lucaszanier@gmail.com>
2022-03-25 19:19:46 -0300

    fusionar?

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const methodOverride =  require('method-override'); 
+const app = express();
 
 
-app.set("view engine", "ejs");
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 
 
-
-let port = 3030;
-
-app.listen(port, function() { 
-    console.log("Server Online!")
-    console.log("Port: "+port)
-})
-
+app.set("view engine", "ejs");
 
 
 var mainRouter = require('./routes/mainRoutes');
 app.use('/', mainRouter);
-
 
 var productRouter = require('./routes/productRoutes');
 app.use('/Product', productRouter);
@@ -34,54 +25,8 @@ var adminRouter = require('./routes/adminRoutes');
 app.use('/Admin', adminRouter);
 
 
-//app.get("/Crear-Editar", function (req, res){
-//    res.render(path.join(__dirname, './views/admin/create_edit.ejs'))
-//})
-//
-//app.get("/Crear", function (req, res){
-//    res.render(path.join(__dirname, './views/admin/create.ejs'))
-//})
-//
-//app.get("/Editar", function (req, res){
-//    res.render(path.join(__dirname, './views/admin/edit.ejs'))
-//})
-
-
-/*
-app.get("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/products/home.html'));
-});
-
-
-app.get("/login", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './src/views/users/login.html'));
-});
-
-app.get("/registrarse", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './src/views/users/register.html'));
-});
-
-app.get("/preregistro", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './src/views/users/preregister.html'));
-});
-
-app.get('/Product-Detail', (req, res) => {
-    res.sendFile(__dirname + '/src/views/products/productDetail2.html');
-});
-
-app.get('/Carrito', (req, res) => {
-    res.sendFile(__dirname + '/src/views/products/shopping.html');
-});
-
-app.get('/Nosotros', (req, res) => {
-    res.sendFile(__dirname + '/src/views/products/us.html');
-});
-
-app.get('/Term&Cond', (req, res) => {
-    res.sendFile(__dirname + '/src/views/users/terminos_condiciones.html');
-});
-
-app.get('/Privacidad', (req, res) => {
-    res.sendFile(__dirname + '/src/views/users/aviso_privacidad.html');
-});
-*/
+let port = 3030;
+app.listen(port, function() { 
+    console.log("Server Online!")
+    console.log("Port: "+port)
+})
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const multer  = require('multer'); //
 const userController = require("../controllers/userController");
 
+const router = express.Router();
 
 router.get('/login', userController.login);
 router.get('/registrarse', userController.register);
