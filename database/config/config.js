5a0d6286f93669edfde2b5aaa390aaf53869869e
Lis Perdomo <ing.lisperdomo@gmail.com>
2022-04-24 21:25:28 -0300

    configurando sequelize y database

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.exports= {
+  "development": {
+    "username": "grupo1",
+    "password": "grupo1",
+    "database": "roadtripping",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
