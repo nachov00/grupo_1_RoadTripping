89550f501007583ab7bd5eff891b68e21e69d8d2
V41KNU7 <lucaszanier@gmail.com>
2022-04-25 21:14:21 -0300

    Product Services

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,13 +1,16 @@
-const services=require('../services/productServices')
+const services = require('../services/productServices')
 
 const productController = {
-  listado: (req, res) => {
-    res.render("products/productList", {productos:services.findAll()})
-  },
+
+    listado: (req, res) => {
+      res.render("products/productList", { productos: services.list() })
+    },
+
     detail: (req, res) => {
       let producto = services.findID(req.params.id)
       res.render("products/productDetail2", {producto})
     },
+
     shopping: (req, res) => {
       res.render("products/shopping")
     },
---
+++ src/services/productServices.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+const moment = require('moment');
+const res = require('express/lib/response');
+
+const productos = db.productos
+
+const productsServices = {
+
+    'list': () => { 
+        db.productos.findAll()
+
+        .then( productos => productos)
+    },
+
+    'detail': () => {
+        db.productos.findByPk(req.params.id)
+
+        .then(producto => producto)
+    },
+
+    //'new': () => {},
+
+    //'recommended': () => {},
+
+    //'add': => {},
+
+    'create': (req, res) => {
+        db.productos.create(
+            {
+                titulo: req.body.titulo,
+                destino: req.body.destino,
+                categoria: req.body.categoria,
+                precio: req.body.precio,
+                detalle: req.body.detalle,
+            }
+        )
+        .then( 
+            () => {return res.redirect('/')} 
+            )
+        .catch(error => res.send(error))
+    },
+
+    /*
+    'edit': (req, res) => {
+        let productoID = req.params.id;
+        let producto = db.productos.findByPk(productoID)
+        Promise.all([producto])
+        .then( producto => {
+
+        } )
+    }, 
+    */
+
+    'update': (req, res) => {
+        let productoID = req.params.id;
+
+        db.producto.update({
+            titulo: req.body.titulo,
+            destino: req.body.destino,
+            categoria: req.body.categoria,
+            precio: req.body.precio,
+            detalle: req.body.detalle,
+            },
+            {
+                where: {id: productoID}
+            })
+        .then( () => {
+            return res.redirect('/')
+        })
+        .catch(error => res.send(error))
+    },
+
+    //'delete': () => {}
+
+    'destroy': (req, res) => {
+        let productoID = req.params.id
+
+        db.productos.destroy( {where: {id: productoID}, force: true} )
+        .then( () => res.redirect('/') )
+        .catch(error => res.send(error))
+    },
+}
+
+module.exports = productsServices;
+
