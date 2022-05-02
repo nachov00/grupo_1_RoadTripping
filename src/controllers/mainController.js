03cc81eff4a3ebbe9265446559c49797ac8f86f4
V41KNU7 <lucaszanier@gmail.com>
2022-05-02 16:03:36 -0300

    nothingness

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,15 +1,27 @@
+const cookies = require('../middleware/recordar')
+
+const req = require("express/lib/request");
+const fs = require("fs");
+const path = require("path")
+
 let userID = req.cookies.user;
+
 const service = require('../services/userServices')
+service.requires();
+
 
 const mainController = {
     home: (req, res) => {
 
-      if( userID ) { 
+  service.requires();
+
+
+    if( userID ) { 
         let userFound = service.findID(ID)
         return ID == userID
       }
 
-      res.render("main/home" , {userFound})
+      res.render("main/home")// , {userFound})
     },
     us: (req, res) => {
       res.render("main/us")
---
+++ src/services/userServices.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'requires': () => {
+        const req = require("express/lib/request");
+        const fs = require("fs");
+        const path = require("path")
+    }
+}
